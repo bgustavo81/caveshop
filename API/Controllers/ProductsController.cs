ad2f1df17e8e4ed77fdc3767b5a583c319b34a6c
Gustavo Bautista <bgustavo81@gmail.com>
2025-02-27 12:09:48 -0500

    worked on products controller

--- API/Controllers/ProductsController.cs
+++ API/Controllers/ProductsController.cs
@@ -1,4 +1,5 @@
 using System;
+using API.RequestHelpers;
 using CORE.Entities;
 using CORE.Interfaces;
 using CORE.Specifications;
@@ -8,17 +9,15 @@ using Microsoft.EntityFrameworkCore;
 
 namespace API.Controllers;
 
-[ApiController]
-[Route("api/[controller]")]
-public class ProductsController(IGenericRepository<Product> repo) : ControllerBase
+
+public class ProductsController(IGenericRepository<Product> repo) : BaseApiController
 {
 
   [HttpGet]
-  public async Task<ActionResult<IReadOnlyList<Product>>> GetProducts(string? brand, string? type, string? sort)
+  public async Task<ActionResult<IReadOnlyList<Product>>> GetProducts([FromQuery] ProductSpecParams specParams)
   {
-    var spec = new ProductSpecification(brand, type, sort);
-    var products = await repo.ListAsync(spec);
-    return Ok(products);
+    var spec = new ProductSpecification(specParams);
+    return await CreatePagedResult(repo, spec, specParams.PageIndex, specParams.PageSize);
   }
 
   [HttpGet("{id:int}")] // api/products/:id
--- Infrastructure/obj/Debug/net8.0/Infrastructure.AssemblyInfo.cs
+++ Infrastructure/obj/Debug/net8.0/Infrastructure.AssemblyInfo.cs
@@ -13,7 +13,7 @@ using System.Reflection;
 [assembly: System.Reflection.AssemblyCompanyAttribute("Infrastructure")]
 [assembly: System.Reflection.AssemblyConfigurationAttribute("Debug")]
 [assembly: System.Reflection.AssemblyFileVersionAttribute("1.0.0.0")]
-[assembly: System.Reflection.AssemblyInformationalVersionAttribute("1.0.0+645d2e26a173e046483ee580e7bbd8b2062e101f")]
+[assembly: System.Reflection.AssemblyInformationalVersionAttribute("1.0.0+dab0e942b925fca9e8464832b981e62a1a8c9076")]
 [assembly: System.Reflection.AssemblyProductAttribute("Infrastructure")]
 [assembly: System.Reflection.AssemblyTitleAttribute("Infrastructure")]
 [assembly: System.Reflection.AssemblyVersionAttribute("1.0.0.0")]
